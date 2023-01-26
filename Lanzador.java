176416276f9c9ca1291347d60d59947c39801985
Hans Davila <hansricardodavila@outlook.com>
2023-01-25 21:47:04 -0600

    Clases MVC agregadas

---
+++ Lanzador.java
@@ -0,0 +1,10 @@
+package mx.com.cursodia.jse18.mod2.semana1.tarea;
+
+public class Lanzador {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Modelo.java
@@ -0,0 +1,5 @@
+package mx.com.cursodia.jse18.mod2.semana1.tarea;
+
+public class Modelo {
+
+}
