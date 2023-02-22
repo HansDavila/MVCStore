fcec312422282b6f5d279eb8c18604545b9bfc57
Hans Davila <hansricardodavila@outlook.com>
2023-02-22 02:42:38 -0600

    Interfaces implementadas en controlador

--- Modelo.java
+++ Modelo.java
@@ -1,5 +1,7 @@
 package mx.com.cursodia.jse18.mod2.semana1.tarea;
 
+
+import interfacesEngine.FiltroEngineRoute;
 import jugueteria.Juguete;
 import jugueteria.Jugueteria;
 
@@ -7,14 +9,16 @@ public class Modelo
 {
 
 	//Objeto de libreria que se encarga del manejo de la base de datos
-	Jugueteria J = new Jugueteria();
+	FiltroEngineRoute J = new Jugueteria();
+	
 
 	
 	public Modelo() 
 	{
 		//METODO Que recibe strings con los objetos del archivo de texto y los convierte en objetos juguetes
-		J.imprimirURL();
+		//J.imprimirURL();
 		J.readRegistrosFile();
+		
 	}
 	
 	Juguete getJuguete(int index) 
@@ -50,7 +54,7 @@ public class Modelo
 	
 	void recorrerRegistro() 
 	{
-		J.recorrerRegistro();
+		J.readToys();
 	}
 	
 	void modifyToy(int index, Juguete modifiedToy) 
@@ -78,40 +82,3 @@ public class Modelo
 	
 }
 
-
-
-
-/*
-public static void main(String[] args) {
-	ArrayList<Integer> list = new ArrayList<Integer>(5);
-	 
-    // using add() to initialize values
-    list.add(1);
-    list.add(2);
-    list.add(3);
-    list.add(4);
-
-    // printing initial value ArrayList
-    System.out.print("ArrayList: " + list);
-
-    // find first element
-    int first = list.get(0);
-    int second = list.get(1);
-    int third = list.get(2);
-
-    // find last element
-    int last = list.get(list.size() - 1);
-
-    // print first and last element of ArrayList
-    System.out.println("\nFirst : " + first + " Second " + second + "Third" + third
-                       + ", Last : " + last);
-}
-
-
-
-1,Pelota,100.0,MexiToys,Basico,100
-2,Transformer,600.0,Hasbro,Accion,20
-3,Barbie,600.0,Matel,Muñeca,40
-4,Lego Batman,1200.0,Lego,Construccion,55
-
-*/
