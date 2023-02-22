d9ae6a4b15cfe81ebfc44ed0386fa5ce4a1897a4
Hans Davila <hansricardodavila@outlook.com>
2023-02-22 00:54:58 -0600

    Version estable

--- Modelo.java
+++ Modelo.java
@@ -13,7 +13,7 @@ public class Modelo
 	public Modelo() 
 	{
 		//METODO Que recibe strings con los objetos del archivo de texto y los convierte en objetos juguetes
-		J.imprimirURL();
+		//J.imprimirURL();
 		J.readRegistrosFile();
 	}
 	
@@ -81,37 +81,3 @@ public class Modelo
 
 
 
-/*
-public static void main(String[] args) {
-	ArrayList<Integer> list = new ArrayList<Integer>(5);
-	 
-    // using add() to initialize values
-    list.add(1);
-    list.add(2);
-    list.add(3);
-    list.add(4);
-
-    // printing initial value ArrayList
-    System.out.print("ArrayList: " + list);
-
-    // find first element
-    int first = list.get(0);
-    int second = list.get(1);
-    int third = list.get(2);
-
-    // find last element
-    int last = list.get(list.size() - 1);
-
-    // print first and last element of ArrayList
-    System.out.println("\nFirst : " + first + " Second " + second + "Third" + third
-                       + ", Last : " + last);
-}
-
-
-
-1,Pelota,100.0,MexiToys,Basico,100
-2,Transformer,600.0,Hasbro,Accion,20
-3,Barbie,600.0,Matel,Muñeca,40
-4,Lego Batman,1200.0,Lego,Construccion,55
-
-*/
